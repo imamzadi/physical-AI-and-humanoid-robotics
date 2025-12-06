6c0adf240746391671bc35329afdabf7e3134e5d
imamzadi <imamzadieman678@gmail.com>
2025-12-06 21:22:32 +0500

    edit comma

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'imamzadi', // Usually your GitHub org/user name.
-  projectName: "physical-AI-and-humanoid-robotics"
+  projectName: "physical-AI-and-humanoid-robotics",
 
   onBrokenLinks: 'throw',
 
